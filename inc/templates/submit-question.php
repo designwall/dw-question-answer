306bd6404ac76dbb6c7d66f859d0653b6442a340
Jackie Lord <j4ckielord@gmail.com>
2013-12-04 15:55:27 +0700

    First pages commit

--- inc/templates/submit-question.php
+++
@@ -1,9 +0,0 @@
-<?php get_header('dwqa'); ?>
-
-<?php do_action( 'dwqa_before_page' ) ?>
-
-    <?php dwqa_load_template('submit-question', 'form' ); ?>
-
-<?php do_action( 'dwqa_after_page' ) ?>
-
-<?php get_footer('dwqa'); ?>

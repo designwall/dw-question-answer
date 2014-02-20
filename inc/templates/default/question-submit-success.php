6c58a8a949428ab70439ff00d798f8822df1f410
bighug <hungnq@joomsolutions.com>
2014-02-20 11:27:44 +0700

    Update Class Message

--- inc/templates/default/question-submit-success.php
+++ inc/templates/default/question-submit-success.php
@@ -6,7 +6,7 @@
 	add_filter( 'posts_where', array($temp_filter, 'posts_where')  );
 ?>
 <div id="archive-question" class="dw-question">
-	<div class="message-success">
+	<div class="message-success alert alert-success">
 		<p>Welldone, you question “<a href="<?php echo get_permalink( $new_question ); ?>"><?php echo get_the_title( $new_question ); ?></a>” succesfully posted  <?php if( isset($tags[0]) ) { echo '<a href="'.get_term_link( $tags[0] ).'" >'.$tags[0]->name.'</a>'; } ?></p>  
 	</div>
 	<div class="dwqa-list-question suggest-question">

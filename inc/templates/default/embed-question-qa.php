dabc86a401d4f2504b50e2178396386f84ad3af1
kido-d <dungpt@joomsolutions.com>
2014-02-27 16:13:31 +0700

    style embed question

--- inc/templates/default/embed-question-qa.php
+++ inc/templates/default/embed-question-qa.php
@@ -1,1 +1,76 @@
-<?php the_content(); ?>
+<?php //the_content(); ?>
+<style>
+.dwqa-question-embed {
+	clear: both;
+	position: relative;
+	border: 1px solid;
+	border-color: #e9e9e9 #e2e2e2 #d1d1d1;
+	-webkit-box-shadow: 0 1px 3px -1px rgba(0, 0, 0, .15);
+	min-height: 150px;
+	margin: 5px 0;
+	position: relative;
+}
+.dwqa-embed-avatar {
+	float: left;
+	margin-right: 15px;
+}
+@media(max-width: 480px) {
+	.dwqa-embed-avatar {
+		float: none;
+		text-align: center;
+	}
+}
+.dwqa-embed-content {
+	display: block;
+	font-size: 12px;
+	padding: 10px 12px 0;
+	position: relative;
+	min-height: 135px;
+}
+.dwqa-embed-title {
+	font-size: 18px;
+	font-weight: 500;
+	line-height: 22px;
+	margin-bottom: 8px;
+	overflow: hidden;
+	word-wrap: break-word;
+}
+.dwqa-embed-title a {
+	text-decoration: none;
+	border-bottom: none !important;
+}
+.dwqa-embed-title a:hover {
+	border-bottom: 1px dotted #b3b3b3 !important;
+}
+.dwqa-embed-summary {
+	line-height: 16px;
+	margin-top: 0;
+	overflow: hidden;
+}
+.dwqa-embed-links {
+	bottom: 0;
+	left: 165px;
+	position: absolute;
+}
+.dwqa-embed-links a {
+	color: #ababab !important;
+	text-transform: uppercase;
+	font-size: 11px;
+	border-bottom: none!important;
+}
+.dwqa-embed-links a:hover {
+	border-bottom: 1px solid #b3b3b3!important;
+}
+</style>
+<div class="dwqa-question-embed">
+	<div class="dwqa-embed-avatar">
+		<img src="http://placehold.it/150x150">
+	</div>
+	<div class="dwqa-embed-content">
+		<div class="dwqa-embed-title"><a href="#">How do I delete the date and author description</a></div>
+		<div class="dwqa-embed-summary">Hi, How do I delete the date and author description on the related posts at the bottom of each post...</div>
+		<div class="dwqa-embed-links">
+	    		<a href="#"><strong>1</strong> answer</a>&nbsp;&nbsp;<a href="#"><strong>20</strong> views</a>
+		</div>
+	</div>
+</div>

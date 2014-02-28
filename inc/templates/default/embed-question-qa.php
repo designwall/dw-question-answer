6eefe4b635481181e0526d78c22abad9ecb899fb
kido-d <dungpt@joomsolutions.com>
2014-02-28 17:51:50 +0700

    update embed qa

--- inc/templates/default/embed-question-qa.php
+++ inc/templates/default/embed-question-qa.php
@@ -1,6 +1,9 @@
 <div class="dwqa-question-embed">
 	<div class="dwqa-embed-avatar">
-        <?php  global $post; echo get_avatar( $post->post_author, 100 ); ?>
+        <?php  global $post; echo get_avatar( $post->post_author, 64 ); ?>
+    </div>
+	<div class="dwqa-embed-content">
+		<div class="dwqa-embed-title"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></div>
         <div class="dwqa-embed-links">
                 <a target="_blank" href="<?php echo get_permalink(); ?>"><?php
                     $answer_count = dwqa_question_answers_count();
@@ -26,12 +29,11 @@
                     }
                 ?></a>
         </div>
-    </div>
-	<div class="dwqa-embed-content">
-		<div class="dwqa-embed-title"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></div>
-		<div class="dwqa-embed-summary"><?php
-			$content =  substr(get_the_content(), 0 , 153 );
-			echo $content . '...';
-		?></div>
 	</div>
+    <div class="dwqa-embed-summary">
+    <?php
+        $content =  substr(get_the_content(), 0 , 153 );
+        echo $content . '...';
+    ?>
+    </div>
 </div>

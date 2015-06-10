5b4547d426a0bb3c6c2cca8c76b66d957a692517
Yehuda Korotkin <yehuda_k@athenaiss.com>
2015-06-10 19:44:18 +0300

    Changes with RTL

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -238,6 +238,9 @@ function dwqa_enqueue_scripts(){
 
 	// Enqueue style
 	wp_enqueue_style( 'dwqa-style', $assets_folder . 'css/style.css', array(), $version );
+	if (is_rtl()) {
+	  wp_enqueue_style( 'dwqa-style', $assets_folder . 'css/rtl.css', array(), $version );
+	}
 	// Enqueue for single question page
 	if ( is_single() && 'dwqa-question' == get_post_type() ) {
 		// js

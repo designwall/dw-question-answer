3077ffe03704e8f583622a321f31f542ebed757d
Yehuda Korotkin <yehuda@korotkin.co.il>
2015-06-10 22:14:54 +0300

    Remove ugly user image (avatar)

--- inc/templates/default/content-comment.php
+++ inc/templates/default/content-comment.php
@@ -18,7 +18,9 @@
 			
 			<span class="dwqa-date">
 				<?php  
-					//echo get_avatar( $comment->user_id, 0 );
+					
+					//  echo get_avatar( $comment->user_id, 0 ); - it's ugly
+					
 					printf( '<strong>&sdot;</strong> <a href="#comment-%1$d" title="%2$s #%1$d">%3$s</a>',
 						$comment->comment_ID,
 						__( 'Link to comment', 'dwqa' ),

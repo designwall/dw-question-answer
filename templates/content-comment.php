b5ab889929229349fbd5204e3ada8fa4efea709e
thanhluu <thanhlt90@gmail.com>
2016-01-26 12:02:42 +0700

    content comment

--- templates/content-comment.php
+++ templates/content-comment.php
@@ -11,7 +11,7 @@
 <div class="dwqa-comment">
 	<div class="dwqa-comment-meta">
 		<a href="<?php echo get_author_posts_url( $comment->user_id ); ?>"><?php the_author_meta( 'display_name', $comment->user_id ); ?></a>
-		<?php printf( _x( '%s ago', '%s = human-readable time difference', 'your-text-domain' ), human_time_diff( get_comment_time( 'U' ), current_time( 'timestamp' ) ) ); ?>
+		<?php printf( _x( '%s ago', '%s = human-readable time difference', 'dwqa' ), human_time_diff( get_comment_time( 'U' ), current_time( 'timestamp' ) ) ); ?>
 		<?php edit_comment_link( __( 'Edit', 'dwqa' ), '  ', '' ); ?>
 	</div>
 	<?php comment_text(); ?>

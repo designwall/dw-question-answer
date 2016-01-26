6809097cf2ce96e7d398ea5b35225efd24f27c0a
thanhluu <thanhlt90@gmail.com>
2016-01-26 10:40:29 +0700

    comment content

--- templates/comments.php
+++ templates/comments.php
@@ -7,25 +7,25 @@
  */
 ?>
 
-<?php do_action( 'dwqa_before_comment_form' ) ?>
-<div class="dwqa-comments">
-	<?php do_action( 'dwqa_before_comment_list' ); ?>
-	<div class="dwqa-comments-list">
-		<?php wp_list_comments( array( 'callback' => 'dwqa_question_comment_callback' ) ); ?>
 
-		<?php if ( ! dwqa_is_closed( get_the_ID() ) && dwqa_current_user_can( 'post_comment' ) ) : ?>
-			<?php
-				$args = array(
-					'comment_field' => '<textarea id="comment" name="comment" aria-required="true" placeholder="' . __( 'Write a comment ...', 'dwqa' ).'"></textarea>',
-					'comment_notes_before' => '',
-					'logged_in_as' => '',
-					'comment_notes_after' => '',
-					'id_form' => 'comment_form_' . get_the_ID(),
-				);
-			?>
-			<?php dwqa_comment_form( $args ); ?>
-		<?php endif; ?>
-		<?php do_action( 'dqwa_after_comment_list' ); ?>
+<div class="dwqa-comments">
+	<?php do_action( 'dwqa_before_comments' ) ?>
+	<div class="dwqa-comments-list">
+		<?php do_action( 'dwqa_before_comments_list' ); ?>
+		<?php wp_list_comments( array( 'callback' => 'dwqa_question_comment_callback' ) ); ?>
+		<?php do_action( 'dqwa_after_comments_list' ); ?>
 	</div>
-	<?php do_action( 'dwqa_after_comment_form' ); ?>
+	<?php if ( ! dwqa_is_closed( get_the_ID() ) && dwqa_current_user_can( 'post_comment' ) ) : ?>
+		<?php
+			$args = array(
+				'comment_field' => '<textarea id="comment" name="comment" aria-required="true" placeholder="' . __( 'Write a comment ...', 'dwqa' ).'"></textarea>',
+				'comment_notes_before' => '',
+				'logged_in_as' => '',
+				'comment_notes_after' => '',
+				'id_form' => 'comment_form_' . get_the_ID(),
+			);
+		?>
+		<?php dwqa_comment_form( $args ); ?>
+	<?php endif; ?>
+	<?php do_action( 'dwqa_after_comments' ); ?>
 </div>
--- templates/content-comment.php
+++ templates/content-comment.php
@@ -9,6 +9,10 @@
 
 <?php global $comment; ?>
 <div class="dwqa-comment">
-	<a href="<?php echo get_author_posts_url( $comment->user_id ); ?>"><?php the_author_meta( 'display_name', $comment->user_id ); ?></a>
-	<?php echo get_comment_text(); ?>
+	<div class="dwqa-comment-meta">
+		<a href="<?php echo get_author_posts_url( $comment->user_id ); ?>"><?php the_author_meta( 'display_name', $comment->user_id ); ?></a>
+		<?php printf( _x( '%s ago', '%s = human-readable time difference', 'your-text-domain' ), human_time_diff( get_comment_time( 'U' ), current_time( 'timestamp' ) ) ); ?>
+		<?php edit_comment_link( __( 'Edit', 'dwqa' ), '  ', '' ); ?>
+	</div>
+	<?php comment_text(); ?>
 </div>

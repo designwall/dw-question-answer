94d7b3046ad7a0546ffa4f452eb975166b9474e9
thanhluu <thanhlt90@gmail.com>
2016-01-26 13:56:02 +0700

    style for answer

--- templates/answers.php
+++ templates/answers.php
@@ -14,22 +14,20 @@ $answers = $wp_query->dwqa_answers;
 
 <div class="dwqa-answers">
 	<?php do_action( 'dwqa_before_answers' ) ?>
+	<?php if ( $answers->have_posts() ) : ?>
 	<div class="dwqa-answers-title"><?php _e( '2 Answers', 'dwqa' ) ?></div>
 	<div class="dwqa-answers-list">
-	<?php do_action( 'dwqa_before_answers_list' ) ?>
-	<?php if ( $answers->have_posts() ) : ?>
-		<?php while ( $answers->have_posts() ) : $answers->the_post(); ?>
-			<?php $question_id = get_post_meta( get_the_ID(), '_question', true ) ?>
-			<?php if ( ( 'private' == get_post_status() && ( dwqa_current_user_can( 'edit_answer' ) || dwqa_current_user_can( 'edit_question', $question_id ) ) ) || 'publish' == get_post_status() ) : ?>
-				<?php dwqa_load_template( 'content', 'single-answer' ); ?>
-			<?php endif; ?>
-		<?php endwhile; ?>
-		<?php wp_reset_postdata(); ?>
-	<?php else : ?>
-		<?php dwqa_load_template( 'content', 'none' ) ?>
-	<?php endif; ?>
-	<?php do_action( 'dwqa_after_answers_list' ) ?>
+		<?php do_action( 'dwqa_before_answers_list' ) ?>
+			<?php while ( $answers->have_posts() ) : $answers->the_post(); ?>
+				<?php $question_id = get_post_meta( get_the_ID(), '_question', true ) ?>
+				<?php if ( ( 'private' == get_post_status() && ( dwqa_current_user_can( 'edit_answer' ) || dwqa_current_user_can( 'edit_question', $question_id ) ) ) || 'publish' == get_post_status() ) : ?>
+					<?php dwqa_load_template( 'content', 'single-answer' ); ?>
+				<?php endif; ?>
+			<?php endwhile; ?>
+			<?php wp_reset_postdata(); ?>
+		<?php do_action( 'dwqa_after_answers_list' ) ?>
 	</div>
+	<?php endif; ?>
 	<?php dwqa_load_template( 'answer', 'submit-form' ) ?>
 	<?php do_action( 'dwqa_after_answers' ); ?>
 </div>

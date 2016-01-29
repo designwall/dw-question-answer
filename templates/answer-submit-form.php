2c0de1fee947ba1c7ee6c2aed919f103801e06db
thanhluu <thanhlt90@gmail.com>
2016-01-29 18:21:48 +0700

    fix bug

--- templates/answer-submit-form.php
+++ templates/answer-submit-form.php
@@ -13,17 +13,16 @@
 	<form name="dwqa-answer-form" id="dwqa-answer-form" method="post">
 		<?php dwqa_print_notices(); ?>
 		<?php dwqa_init_tinymce_editor( array( 'textarea_name' => 'answer-content', 'id' => 'dwqa-answer-content' ) ) ?>
+		<?php dwqa_load_template( 'captcha', 'form' ); ?>
 		<select class="dwqa-select" name="dwqa-status">
 			<optgroup label="<?php _e( 'Who can see this?', 'dwqa' ) ?>">
 				<option value="publish"><?php _e( 'Public', 'dwqa' ) ?></option>
 				<option value="private"><?php _e( 'Only Me &amp; Admin', 'dwqa' ) ?></option>
 			</optgroup>
 		</select>
-		<!-- <input type="checkbox" name="private" value="1"> -->
 		<input type="submit" name="submit-answer" class="dwqa-btn dwqa-btn-primary" value="<?php _e( 'Submit', 'dwqa' ) ?>">
 		<input type="hidden" name="question_id" value="<?php the_ID(); ?>">
 		<input type="hidden" name="dwqa-action" value="add-answer">
-		<?php dwqa_load_template( 'captcha', 'form' ); ?>
 		<?php wp_nonce_field( '_dwqa_add_new_answer' ) ?>
 	</form>
 	<?php do_action( 'dwqa_after_answer_submit_form' ); ?>
--- templates/single-question.php
+++ templates/single-question.php
@@ -10,18 +10,6 @@
 <div class="dwqa-single-question">
 <?php if ( have_posts() ) : ?>
 	<?php do_action( 'dwqa_before_single_question' ) ?>
-
-	<?php if ( dwqa_current_user_can( 'edit_question', get_the_ID() ) ) : ?>
-	<?php _e( 'This question is:', 'dwqa' ) ?>
-	<select id="dwqa-question-status" data-nonce="<?php echo wp_create_nonce( '_dwqa_update_privacy_nonce' ) ?>" data-post="<?php the_ID(); ?>">
-		<optgroup label="Status">
-			<option <?php selected( dwqa_question_status(), 'open' ) ?> value="open"><?php _e( 'Open', 'dwqa' ) ?></option>
-			<option <?php selected( dwqa_question_status(), 'close' ) ?> value="close"><?php _e( 'Close', 'dwqa' ) ?></option>
-			<option <?php selected( dwqa_question_status(), 'resolved' ) ?> value="resolved"><?php _e( 'Resolve', 'dwqa' ) ?></option>
-		</optgroup>
-	</select>
-	<?php endif; ?>
-
 	<?php while ( have_posts() ) : the_post(); ?>
 		<?php if ( !dwqa_is_edit() ) : ?>
 			<?php dwqa_load_template( 'content', 'single-question' ) ?>

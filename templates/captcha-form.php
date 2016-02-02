8c21209c1c6e3ab410b82a958a909194032c3558
ORYC <oryc9x@gmail.com>
2016-02-02 17:30:07 +0700

    fix captcha

--- templates/captcha-form.php
+++ templates/captcha-form.php
@@ -8,7 +8,7 @@
 	<span class="dwqa-plus">&#43;</span>
 	<span class="dwqa-number-one"><?php echo esc_attr( $number_2 ) ?></span>
 	<span class="dwqa-plus">&#61;</span>
-	<input type="text" name="dwqa-captcha-result" id="dwqa-captcha-result" value="">
+	<input type="text" name="dwqa-captcha-result" id="dwqa-captcha-result" value="" placeholder="<?php _e( 'Enter the result', 'dwqa' ) ?>">
 	<input type="hidden" name="dwqa-captcha-number-1" id="dwqa-captcha-number-1" value="<?php echo esc_attr( $number_1 ) ?>">
 	<input type="hidden" name="dwqa-captcha-number-2" id="dwqa-captcha-number-2" value="<?php echo esc_attr( $number_2 ) ?>">
 </p>

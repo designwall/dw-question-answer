8d28a0ece82edcdfdf161e92a0d7be82822720b4
Arf <adrientichoux@gmail.com>
2018-03-05 12:06:26 +0100

    Update Latest_Question.php

--- inc/widgets/Latest_Question.php
+++ inc/widgets/Latest_Question.php
@@ -41,7 +41,7 @@ class DWQA_Widgets_Latest_Question extends WP_Widget {
 				echo '</a>';
 				echo __( 'asked by', 'dwqa' ) . ' ' . get_the_author_link();
 				if ( isset( $instance['question_date'] ) && $instance['question_date'] ) {
-					echo ', ' . human_time_diff( get_the_time( 'U', true ) ) . ' ' . __( 'ago', 'dwqa' );
+					echo ', ' . human_time_diff( get_the_time( 'U' ) ) . ' ' . __( 'ago', 'dwqa' );
 				}
 				echo '</li>';
 			}   
